955fb607ea2e6991ed94ea6f7be93c4af1afd997
will-mztt <william@mztt.com.br>
2024-11-15 12:27:29 -0300

    Working VSCode and Chrome debugging config.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Svelte & SvelteKit
+
+This is a sample base project using Svelte 5.0 and SvelteKit 2.0 without TypeScript support and using `pnpm` as package manager.
+
+## Debugging on VS Code
+
+Debugging client-side code on VS Code is functional on this version of the project.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+
+    console.log("Hello world");
+    let count = $state(0);
+
+    function increment() {
+        count += 1;
+    }
+    // const increment = () => (count += 1);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+<button onclick={increment}>
+    {count}
+</button>
